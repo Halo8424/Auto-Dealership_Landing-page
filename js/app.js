06d717fb6942a8466b6a3a564b925f6f8bdb66bf
Richard.C <r.conde@live.com>
2021-09-27 15:20:41 -0400

    loadFirstSlide function created

--- js/app.js
+++ js/app.js
@@ -1,22 +1,28 @@
-let init = () =>{
-    let container = document.getElementsByClassName("jumbo-slider__container")[0],
+// Global variables
+let container = document.getElementsByClassName("jumbo-slider__container")[0],
     jumboSliderSlidesArry = document.getElementsByClassName("jumbo-slider__slide"),
     jumboSliderElipsisCirclesArry = document.getElementsByClassName("jumbo-slider__circle"),
     jumboSliderNavLinksArry = document.getElementsByClassName("jumbo-slider__link"),
     activeSlide = 1,
     slideRunTime = 6000;
 
+// Functions
+let loadFirstSlide = ()=>{
     jumboSliderSlidesArry[0].classList.add("jumbo-slider__slide--active");
+};
 
-    jumboSliderNavLinksArry[activeSlide-1].classList.add("jumbo-slider__link--active");
+let init = () =>{
+    loadFirstSlide();
 
-    let updateNav = () => {
-        console.log(`Current: ${activeSlide}`);
-    };
+    // jumboSliderNavLinksArry[activeSlide-1].classList.add("jumbo-slider__link--active");
 
-    let startSlider = () => {
-        
-    };
+    // let updateNav = () => {
+    //     console.log(`Current: ${activeSlide}`);
+    // };
+
+    // let startSlider = () => {
+
+    // };
 };
 
 // init();
