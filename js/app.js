b6b7f99084587997012cc16ced2a3bf66457ef20
Richard.C <r.conde@live.com>
2021-09-27 12:54:58 -0400

    targeting the active slide and the slide run time

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 let init = () =>{
     let container = document.getElementsByClassName("jumbo-slider__container")[0],
     jumboSliderSlidesArry = document.getElementsByClassName("jumbo-slider__slides"),
-    jumboSliderElipsisCirclesArry = document.getElementsByClassName("jumbo-slider__circle")
+    jumboSliderElipsisCirclesArry = document.getElementsByClassName("jumbo-slider__circle"),
+    jumboSliderNavLinksArry = document.getElementsByClassName("jumbo-slider__link"),
+    activeSlide = 1,
+    slideRunTime = 6000;
+
 };
 
 init();
