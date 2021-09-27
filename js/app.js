6ac645483ea50cad0ddb72b6fc822da99f9b4092
Richard.C <r.conde@live.com>
2021-09-27 13:06:19 -0400

    fix to video not loading: incorrect spelling on jumbo-slider-slide

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 let init = () =>{
     let container = document.getElementsByClassName("jumbo-slider__container")[0],
-    jumboSliderSlidesArry = document.getElementsByClassName("jumbo-slider__slides"),
+    jumboSliderSlidesArry = document.getElementsByClassName("jumbo-slider__slide"),
     jumboSliderElipsisCirclesArry = document.getElementsByClassName("jumbo-slider__circle"),
     jumboSliderNavLinksArry = document.getElementsByClassName("jumbo-slider__link"),
     activeSlide = 1,
     slideRunTime = 6000;
 
+    jumboSliderSlidesArry[0].classList.add("jumbo-slider__slide--active");
 };
 
 init();
