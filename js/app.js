f1da50852532950eef9a2bd1cfb8eab58dd2c089
Richard.C <r.conde@live.com>
2021-09-27 12:46:57 -0400

    target of jumbo-slider__slides and jumbo-slider__circle

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let init = () =>{
     let container = document.getElementsByClassName("jumbo-slider__container")[0],
-    
+    jumboSliderSlidesArry = document.getElementsByClassName("jumbo-slider__slides"),
+    jumboSliderElipsisCirclesArry = document.getElementsByClassName("jumbo-slider__circle")
 };
 
 init();
