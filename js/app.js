a1af8f7be3a2c7798f92115adab8b24d3a248494
Richard.C <r.conde@live.com>
2021-09-27 15:38:01 -0400

    startSlider function passing test

--- js/app.js
+++ js/app.js
@@ -1,34 +1,48 @@
 // Global variables
 let container = document.getElementsByClassName("jumbo-slider__container")[0],
-    jumboSliderSlidesArry = document.getElementsByClassName("jumbo-slider__slide"),
-    jumboSliderElipsisCirclesArry = document.getElementsByClassName("jumbo-slider__circle"),
-    jumboSliderNavLinksArry = document.getElementsByClassName("jumbo-slider__link"),
-    activeSlide = 1,
-    slideRunTime = 6000;
+  jumboSliderSlidesArry = document.getElementsByClassName(
+    "jumbo-slider__slide"
+  ),
+  jumboSliderElipsisCirclesArry = document.getElementsByClassName(
+    "jumbo-slider__circle"
+  ),
+  jumboSliderNavLinksArry =
+    document.getElementsByClassName("jumbo-slider__link"),
+  activeSlide = 1,
+  slideRunTime = 6000;
 
 // Functions
-let loadFirstSlide = ()=>{
-    jumboSliderSlidesArry[0].classList.add("jumbo-slider__slide--active");
+let loadFirstSlide = () => {
+  jumboSliderSlidesArry[0].classList.add("jumbo-slider__slide--active");
 };
 
-let highLightFirstLink = ()=>{
-    jumboSliderNavLinksArry[activeSlide-1].classList.add("jumbo-slider__link--active");
+let highLightFirstLink = () => {
+  jumboSliderNavLinksArry[activeSlide - 1].classList.add(
+    "jumbo-slider__link--active"
+  );
 };
 
 let updateNavOnSlideChange = () => {
-    console.log(`Current: ${activeSlide}`);
+  console.log(`Current: ${activeSlide}`);
 };
 
-let init = () =>{
-    loadFirstSlide();
-    highLightFirstLink();
-    updateNavOnSlideChange();
+let startSlider = () => {
+    let test = 1;
+  setInterval(() => {
+    console.log(test);
+    test++;
+  }, 6000)
+};
 
-    
+let init = () => {
+  loadFirstSlide();
+  highLightFirstLink();
+  updateNavOnSlideChange();
+  startSlider();
 
-    // let startSlider = () => {
+  // let startSlider = () => {
 
-    // };
+  // };
 };
 
 // init();
